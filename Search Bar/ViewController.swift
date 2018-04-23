7a95081ee3fbd161cee4e4b5b047d72964a0671c
Chien <nguyenquangchien3010@gmail.com>
2018-04-23 14:24:59 +0700

    init

---
+++ Search Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search Bar
+//
+//  Created by Chien on 3/19/18.
+//  Copyright © 2018 Chien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
